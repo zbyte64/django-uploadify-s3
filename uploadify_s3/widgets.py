1d77ee55d5180ffc5f7a692323f0bf51f5a644b2
Jason Kraus <jasonk@cukerinteractive.com>
2012-02-23 16:47:08 -0800

    removed dupe saving

--- uploadify_s3/widgets.py
+++ uploadify_s3/widgets.py
@@ -1,6 +1,8 @@
 from django.forms.widgets import FileInput, ClearableFileInput
 from django.core.files.storage import default_storage
 
+from django.db.models.fields.files import FieldFile, FileField
+
 class UploadifyFileInput(FileInput):
     class Media: #this does not work for the admin as django ignores it [WTF]
         css = ('uploadify/uploadify.css',)
@@ -11,7 +13,11 @@ class UploadifyFileInput(FileInput):
         if name in data:
             file_path = data[name]
             #TODO we need to know the storage engine of the file field
-            return default_storage.open(file_path)
+            file_obj = default_storage.open(file_path)
+            file_copy = FieldFile(None, FileField(upload_to='', storage=default_storage), file_path)
+            file_copy.file = file_obj
+            file_copy._committed = True
+            return file_copy
         return super(UploadifyFileInput, self).value_from_datadict(data, files, name)
     
     def render(self, name, value, attrs=None):
@@ -29,7 +35,11 @@ class UploadifyClearableFileInput(ClearableFileInput):
         if name in data:
             file_path = data[name]
             #TODO we need to know the storage engine of the file field
-            return default_storage.open(file_path)
+            file_obj = default_storage.open(file_path)
+            file_copy = FieldFile(None, FileField(upload_to='', storage=default_storage), file_path)
+            file_copy.file = file_obj
+            file_copy._committed = True
+            return file_copy
         return super(UploadifyClearableFileInput, self).value_from_datadict(data, files, name)
     
     def render(self, name, value, attrs=None):
